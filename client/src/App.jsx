6c52daf46a321cd9f42f1008ac4f96fe037f4ee1
Vic428-human <z0983195379a@gmail.com>
2026-02-04 15:29:51 +0800

    feat:規劃router page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Home from "./pages/Home";
+import Marketplace from "./pages/Marketplace";
+import MyListings from "./pages/MyListings";
+import ListingDetails from "./pages/ListingDetails";
+import ManageListing from "./pages/ManageListing";
+import Messages from "./pages/Messages";
+import MyOrders from "./pages/MyOrders";
+import Loading from "./pages/Loading";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/my-listings" element={<MyListings />} />
+        <Route path="/listing/:listingId" element={<ListingDetails />} />
+        <Route path="/crate-listing" element={<ManageListing />} />
+        <Route path="/edit-listing/:id" element={<ManageListing />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
