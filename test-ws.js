458e1376cfebfccb3135580c7198f99964d3071c
Vic428-human <z0983195379a@gmail.com>
2026-02-18 06:42:19 +0800

    移植聊天室功能

---
+++ test-ws.js
@@ -0,0 +1,5 @@
+﻿let ws = new WebSocket('ws://localhost:3000/ws?name=test'); // 當前專案的後端對應的port是3000，因為env環境有另外設定
+ws.onopen = () => { console.log('✅ WS 連線成功'); ws.send('hello'); }
+ws.onmessage = (e) => console.log('收到:', e.data);
+ws.onerror = (e) => console.log('❌ Error:', e);
+ws.onclose = () => console.log('斷線');
